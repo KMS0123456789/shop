00b10e729b98639727eefcf14c12a06364135fb9
KMS0123456789 <kshlover3@naver.com>
2024-08-13 11:54:45 +0900

    addr as

--- Shop/src/main/java/com/project/shop/user/vo/AddrVO.java
+++ Shop/src/main/java/com/project/shop/user/vo/AddrVO.java
@@ -2,34 +2,63 @@ package com.project.shop.user.vo;
 
 public class AddrVO {
 	private int addrNo;
-	private String addrUser;
+	private String addrUserEmail;
 	private String address;
 	private int addrFlag;
-	
+	private String dAddrName;
+	private String addrUserName;
+	private String addrUserPhoneNum;
+	private String addrUserLandLinel;
 	public int getAddrNo() {
 		return addrNo;
-	}
-	public String getAddrUser() {
-		return addrUser;
-	}
-	public String getAddress() {
-		return address;
-	}
-	public int getAddrFlag() {
-		return addrFlag;
 	}
 	public void setAddrNo(int addrNo) {
 		this.addrNo = addrNo;
 	}
-	public void setAddrUser(String addrUser) {
-		this.addrUser = addrUser;
+	public String getAddrUserEmail() {
+		return addrUserEmail;
+	}
+	public void setAddrUserEmail(String addrUserEmail) {
+		this.addrUserEmail = addrUserEmail;
+	}
+	public String getAddress() {
+		return address;
 	}
 	public void setAddress(String address) {
 		this.address = address;
 	}
+	public int getAddrFlag() {
+		return addrFlag;
+	}
 	public void setAddrFlag(int addrFlag) {
 		this.addrFlag = addrFlag;
 	}
+	public String getdAddrName() {
+		return dAddrName;
+	}
+	public void setdAddrName(String dAddrName) {
+		this.dAddrName = dAddrName;
+	}
+	public String getAddrUserName() {
+		return addrUserName;
+	}
+	public void setAddrUserName(String addrUserName) {
+		this.addrUserName = addrUserName;
+	}
+	public String getAddrUserPhoneNum() {
+		return addrUserPhoneNum;
+	}
+	public void setAddrUserPhoneNum(String addrUserPhoneNum) {
+		this.addrUserPhoneNum = addrUserPhoneNum;
+	}
+	public String getAddrUserLandLinel() {
+		return addrUserLandLinel;
+	}
+	public void setAddrUserLandLinel(String addrUserLandLinel) {
+		this.addrUserLandLinel = addrUserLandLinel;
+	}
 	
+	
+
 	
 }
